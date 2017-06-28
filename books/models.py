9489b6d830e552404de2c2531dbbfc4650d26053
Nischal <hi@nischal.info.np>
2017-06-28 23:50:48 +0545

    Adds additional three models Author, Publication and Book.fixes  Bug01: Object was displayed instead of model name by adding __str__ method

--- books/models.py
+++ books/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Publication(models.Model):
+	name = models.CharField("Name of the publiation", max_length = 100)
+
+	def __str__(self):
+		return self.name
+
+class Book(models.Model):
+	title = models.CharField("Title of the Book", max_length = 100)
+	summary = models.TextField("Summary of the book")
+	publication = models.ForeignKey(Publication, on_delete = models.CASCADE)
+	price = models.IntegerField(default = 100)
+	ISBN = models.IntegerField("Enter the ISBN Number")
+	published_date = models.DateTimeField()
+	
+	def __str__(self):
+		return self.title
+
+class Author(models.Model):
+	name = models.CharField("Name of Author", max_length = 100)
+	book = models.ManyToManyField(Book)
+	publication = models.ManyToManyField(Publication)
+
+	def __str__(self):
+		return self.name
