3d4313b3e6bf506bad9b69b480717b57be739fbe
Nischal <hi@nischal.info.np>
2017-06-29 08:36:58 +0545

    Extract data from database and renders with a basic html file, Sucessfully maintains a MVC pattern, ==> BoiledEggs/urls.py => Books/urls.py => Books/views.py => Models.py, doesn't alter forms, basic applicaton

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from books.models import *
 
 # Create your views here.
+
+def book_list(request):
+	books = Book.objects.all()
+	return render(request, 'books/book_list.html', {'books':books})
+
